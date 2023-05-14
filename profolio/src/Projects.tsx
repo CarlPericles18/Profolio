a0414b48224ce82b520f24758dcc8c6ff6bd14c6
Perc <carlpericles18@gmail.com>
2023-05-14 18:17:40 +0900

    First Commit

---
+++ profolio/src/Projects.tsx
@@ -0,0 +1,14 @@
+type HeadingProps = { title: string}
+
+export default function Heading({title}: HeadingProps){
+    return(
+            <div>
+                <button>
+                    Projects
+                </button>
+                <div className="projectContainers">
+                        
+                </div>
+            </div>
+        )
+}
